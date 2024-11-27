69a51205fab14ff2547df346a6cc13bdb056ee0c
Rashidprojects <rashidgitproject@gmail.com>
2024-11-27 12:11:52 +0530

    Pagination setted for Main table also updated ui on dahboard

--- src/components/Dashboard/CustomSelect.jsx
+++ src/components/Dashboard/CustomSelect.jsx
@@ -1,17 +1,27 @@
-import  { useState } from "react";
+import  { useEffect, useState } from "react";
 import { IoIosArrowDropdownCircle,  IoIosArrowDropupCircle } from "react-icons/io";
 
-const CustomSelect = ({ onSelectionChange }) => {
-  const [showoptions, setShowOptions] = useState(false)
-
-  // State for options
-  const [options, setOptions] = useState([
+const CustomSelect = ({ onSelectionChange, reset }) => {
+    const defaultOptions = [
     { id: 0, value: "", name: "Filter users by area", status: true },
     { id: 1, value: "A1", name: "Area 1", status: false },
     { id: 2, value: "A2", name: "Area 2", status: false },
     { id: 3, value: "A3", name: "Area 3", status: false },
     { id: 4, value: "A4", name: "Area 4", status: false },
-  ]);
+  ];
+
+  const [showOptions, setShowOptions] = useState(false);
+  const [options, setOptions] = useState(defaultOptions);
+
+  // Reset options when the reset prop changes
+  useEffect(() => {
+    if (reset) {
+      setOptions(defaultOptions);
+      if (onSelectionChange) {
+        onSelectionChange(""); // Notify parent of reset
+      }
+    }
+  }, [reset]);
 
   // Function to handle click on an option
   const handleOptionClick = (id) => {
@@ -38,20 +48,20 @@ const CustomSelect = ({ onSelectionChange }) => {
           (item) =>
             item.status && <p key={item.id} className="font-medium sm:text-[18px] px-3" >{item.name}</p> // Only show the active option
         )}
-        <div onClick={() => setShowOptions(!showoptions)}>
+        <div onClick={() => setShowOptions(!showOptions)}>
         {
-            !showoptions ? (<IoIosArrowDropdownCircle className="text-[23px] text-primary"/>) : 
+            !showOptions ? (<IoIosArrowDropdownCircle className="text-[23px] text-primary"/>) : 
             (<IoIosArrowDropupCircle className="text-[23px] text-primary"/>)
         }
         </div>
       </div>
 
       {/* Display all options */}
-      <div className={`flex flex-col ${!showoptions ? 'hidden' : ''}  absolute border border-secondary bg-light w-[230px] sm:w-[240px] rounded-md mt-1`}>
+      <div className={`flex flex-col ${!showOptions ? 'hidden' : ''}  absolute border border-secondary bg-light w-[230px] sm:w-[240px] rounded-md mt-1`}>
         {options.map((item) => (
           <p
             key={item.id}
-            onClick={() => { handleOptionClick(item.id),  setShowOptions(!showoptions) }}
+            onClick={() => { handleOptionClick(item.id),  setShowOptions(!showOptions) }}
             className={` cursor-pointer hover:text-primary sm:text-[20px] py-2 px-4 ${ item.status ? 'bg-secondary text-light' : '' } `} 
           >
             {item.name}
--- src/components/MainTable.jsx
+++ src/components/MainTable.jsx
@@ -1,12 +1,34 @@
 import { useEffect, useState } from 'react';
 import { useForm } from '../lib/providers/FormContext';
 import { useUserData } from '../lib/providers/UserDataContext';
+import { usePagination } from '../lib/providers/PaginationContext';
 
 const MainTable = ({ isArea }) => {
   const { state: formState } = useForm();
   const { state: userState, fetchUsers } = useUserData();
+  const { state, setTotalItems } = usePagination();
+  const { currentPage, itemsPerPage } = state;
 
   const [filteredUsers, setFilteredUsers] = useState([]);
+  
+  // Effect to set the total number of items when filteredUsers changes
+  useEffect(() => {
+    const totalItems = filteredUsers.length;
+
+    // Update context
+    setTotalItems(totalItems);
+
+    // Update local storage
+    localStorage.setItem('filteredUsersCount', totalItems.toString());
+  }, [filteredUsers.length]);
+
+  // Get the data for the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  //   lastItem = 2 * 5 = 10
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  // firstItem = 10 - 5 = 5
+  const currentData = filteredUsers.slice(indexOfFirstItem, indexOfLastItem);
+  // slice data => slice(5, 10) o/p => [6,7,8,9,10]
 
   // Function to filter users by area
   const filterByArea = (areaCode) => {
@@ -39,7 +61,6 @@ const MainTable = ({ isArea }) => {
     }
   }, [userState.users]);
 
-  if (userState.loading) return <p>Loading...</p>;
 
   return (
     <div className="overflow-auto rounded-lg shadow mx-5">
@@ -49,48 +70,48 @@ const MainTable = ({ isArea }) => {
         <table className="w-full">
           <thead className="bg-primary text-light border-2 border-primary ">
             <tr>
-              <th className="w-20 p-3 text-xl font-semibold tracking-wide text-left">No.</th>
-              <th className="w-20 p-3 text-xl font-semibold tracking-wide text-left">Name</th>
-              <th className="w-20 p-3 text-xl font-semibold tracking-wide text-left">House No.</th>
-              <th className="w-20 p-3 text-xl font-semibold tracking-wide text-left">Area Code</th>
-              <th className="w-24 p-3 text-xl font-semibold tracking-wide text-left">Address</th>
-              <th className="w-24 p-3 text-xl font-semibold tracking-wide text-left">Phone</th>
-              <th className="w-24 p-3 text-xl font-semibold tracking-wide text-left">Total</th>
-              <th className="w-24 p-3 text-xl font-semibold tracking-wide text-left">Received</th>
-              <th className="w-32 p-3 text-xl font-semibold tracking-wide text-left">Pending</th>
+              <th className="w-20 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">No.</th>
+              <th className="w-20 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">Name</th>
+              <th className="w-32 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">House No.</th>
+              <th className="w-20 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">Area Code</th>
+              <th className="w-24 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">Address</th>
+              <th className="w-24 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">Phone</th>
+              <th className="w-24 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">Total</th>
+              <th className="w-24 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">Received</th>
+              <th className="w-32 p-2 text-[18px] sm:text-[22px] font-normal tracking-wide text-left">Pending</th>
             </tr>
           </thead>
           <tbody className="border-2 border-primary">
-            {filteredUsers.map((item, index) => (
+            {currentData.map((item, index) => (
               <tr
                 key={item.id}
                 className={`${(index + 1) % 2 === 0 ? 'bg-dark' : 'bg-table1'}`}
               >
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap">
                   {index + 1}
                 </td>
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap">
                   {item.username}
                 </td>
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap w-[30] ">
                   {item.houseNumber}
                 </td>
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap">
                   {item.areaCode}
                 </td>
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap">
                   {item.address}
                 </td>
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap">
                   {item.mobile}
                 </td>
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap">
                   {item.totalAmount}
                 </td>
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap">
                   {item.received}
                 </td>
-                <td className="p-3 text-xl text-gray-700 whitespace-nowrap">
+                <td className="p-2 text-[18px] sm:text-[22px] text-gray-700 whitespace-nowrap">
                   {/* Pending amount */}
                 </td>
               </tr>
